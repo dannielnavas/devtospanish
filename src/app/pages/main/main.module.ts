55bc8b0eb223c23000cc0d8efef4d02b80a26155
Danniel Navas <dannielnavas@gmail.com>
2022-09-22 19:48:35 -0500

    🐛 se elimina importacion

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import CardPostModule from 'src/app/components/card-post/card-post.module';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import MainComponent from './main.component';
 import MainRoutingModule from './main-routing.module';
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [
-    CommonModule,
-    MainRoutingModule,
-    CardPostModule,
-    InfiniteScrollModule,
-  ],
+  imports: [CommonModule, MainRoutingModule, CardPostModule],
 })
 export default class MainModule {}
