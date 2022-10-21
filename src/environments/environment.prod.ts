6b8da5a7658f0cfd619b587be8cf82d05555b988
Danniel Navas <dannielnavas@gmail.com>
2022-10-21 17:05:42 -0500

    🐛 Corrección endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  devtoURL: 'https://devtospanishback.vercel.app',
+  devtoURL: 'https://devtospanishback.vercel.app/api',
 };
 
 export default environment;
