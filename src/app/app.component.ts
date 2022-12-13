1314946c58fec4a74e2d411a873373897ef73749
Danniel Navas <dannielnavas@gmail.com>
2022-12-13 09:53:27 -0500

    🐛  library is remove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
 import LoaderService from './core/services/loader.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export default class AppComponent {
-    title = 'devtospanish';
+  title = 'devtospanish';
 
-    constructor(private loaderService: LoaderService) {
-        this.loaderService.show();
-    }
+  constructor(private loaderService: LoaderService) {
+    this.loaderService.show();
+  }
 }
